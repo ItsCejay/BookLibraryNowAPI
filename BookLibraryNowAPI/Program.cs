6556eff0d61ec86e389ea0f302c2022ce5192d10
Chris <chrisjericoqlucanas@gmail.com>
2026-05-23 17:02:28 +0800

    Update Program.cs

--- BookLibraryNowAPI/Program.cs
+++ BookLibraryNowAPI/Program.cs
@@ -14,8 +14,8 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
-//var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-//app.Urls.Add($"http://*:{port}");
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"http://*:{port}");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
